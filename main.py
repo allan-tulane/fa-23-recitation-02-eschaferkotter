7bc137366ce5523598f7f6911112fbfc3e1814b7
eschaferkotter <123409959+eschaferkotter@users.noreply.github.com>
2023-09-13 03:33:32 +0000

    progress, need readme answers

--- main.py
+++ main.py
@@ -8,52 +8,30 @@ import time
 ###
 
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
-
+  if (n == 1):
+    return 1
+  else:
+    return a*simple_work_calc(n//b,a,b)+n
+  pass
+  
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  if (n == 1):
+    return 1
+  else:
+    return a*work_calc(n//b,a,b,f)+f(n)
+  pass
 
 def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
-
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
-           the work done at each node 
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  if (n == 1):
+    return 1
+  else:
+    return a*(span_calc((n/b),a,b,f))+f(n)
+  pass
 
 
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
+  """
 	Compare the values of different recurrences for 
 	given input sizes.
 
@@ -62,32 +40,47 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 	(n, work_fn1(n), work_fn2(n), ...)
 	
 	"""
-	result = []
-	for n in input_sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
-	return result
+  result = []
+  for n in sizes:
+  # compute W(n) using current a, b, f
+    result.append((
+      n,
+      work_fn1(n),
+      work_fn2(n)
+      ))
+  return result
 
 def print_results(results):
-	""" done """
-	print(tabulate.tabulate(results,
-							headers=['n', 'W_1', 'W_2'],
-							floatfmt=".3f",
-							tablefmt="github"))
+  """ done """
+  print(tabulate.tabulate(results,
+            headers=['n', 'W_1', 'W_2'],
+            floatfmt=".3f",
+            tablefmt="github"))
 
 def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
+  def work_fn1(n):
+    return work_calc(n, 1, 2, lambda n: n*n)
+  def work_fn2(n):
+    return work_calc(n, 3, 2, lambda n: n*n)
+  res = compare_work(work_fn1, work_fn2)
+  print(res)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+  """
+  Compare the values of different recurrences for 
+  given input sizes.
 
-def test_compare_span():
-	# TODO
+  Returns:
+  A list of tuples of the form
+  (n, work_fn1(n), work_fn2(n), ...)
+  
+  """
+  result = []
+  for n in sizes:
+    # compute W(n) using current a, b, f
+    result.append((
+      n,
+      span_fn1,
+      span_fn2
+  ))
+  return result
--- test_main.py
+++ test_main.py
@@ -1,12 +1,15 @@
 from main import *
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
-
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+  assert simple_work_calc(50, 2, 2) == 276
+  assert simple_work_calc(30, 2, 2) == 128
+  assert simple_work_calc(75, 4, 2) == 8255
+  
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  assert work_calc(10, 2, 2,lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n*n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+  assert work_calc(8,2,2,lambda n:n) == 32
